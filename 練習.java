2dbeefaad01426e3364980c373cea1d273e0ecb8
saru928 <kana.softball5@gmail.com>
2024-07-21 22:31:01 +0900

    2024/07/21

---
+++ 練習.java
@@ -0,0 +1,53 @@
+
+public class 練習 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("こんにちは！！");
+		System.out.println(123);
+		System.out.println("おはよう");
+		System.out.println(0);
+		System.out.println("例外が発生しました。");
+		System.out.println("例外が\n発生しました。");
+		
+		String str1 = "鈴木";
+		System.out.println(str1);
+		String tel = "090-1234-5678";
+		System.out.println(tel);
+		char char1 = 'A';
+		System.out.println(char1);
+		int num1 = 12345;
+		System.out.println(num1);
+		boolean boo = true;
+		System.out.println(boo);
+		int a = 3+3;
+		System.out.println(a);
+		String b = "3+3";
+		System.out.println(b);
+		String abc = "山田";
+		System.out.println(abc + "さん");
+		
+		int a1 = 4 + 3;
+		System.out.println(a1);
+		int a2 = -8 - 3;
+		System.out.println(a2);
+		var a3 = -5 * 3;
+		System.out.println(a3);
+		var a4 = 4 / 2;
+		System.out.println(a4);
+		int a5 = 0 / 4;
+		System.out.println(a5);
+		int a6 = 10 / 3;
+		System.out.println(a6);
+		float a7 = (float)10 / 3;
+		System.out.println(a7);
+		double a8 = (double)10 / 3;
+		System.out.println(a8);
+		int a9 = 7 % 3;
+		System.out.println(a9);
+		System.out.println(5 % 3);
+		
+
+	}
+
+}
