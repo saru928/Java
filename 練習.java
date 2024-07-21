5f05a79f3bbfde338ab161728e5a012b81e18fcd
saru928 <kana.softball5@gmail.com>
2024-07-21 23:56:02 +0900

    2024/07/21

--- 練習.java
+++ 練習.java
@@ -47,6 +47,127 @@ public class 練習 {
 		System.out.println(a9);
 		System.out.println(5 % 3);
 		
+		int b1 = 10;
+		System.out.println(++b1);
+		System.out.println(++b1);
+		System.out.println(++b1);
+		System.out.println(++b1);
+		int b2 = 5;
+		System.out.println(b2++);
+		System.out.println(b2++);
+		System.out.println(b2++);
+		System.out.println(b2++);
+		
+		int c1 = 10;
+		System.out.println(--c1);
+		System.out.println(--c1);
+		System.out.println(--c1);
+		System.out.println(--c1);
+		int c2 = 10;
+		System.out.println(c2--);
+		System.out.println(c2--);
+		System.out.println(c2--);
+		System.out.println(c2--);
+		
+		int d1 = 10;
+		if(d1 <20) {
+			System.out.println("A");
+		}
+		String d2 = "山田";
+		if(d2.equals("山田")) {
+			System.out.println(d2 + "さん");
+		}
+		int d3 = 5;
+		if(d3 < 10 && 3 < d3) {
+			System.out.println("A");
+		}
+		int d4 = 5;
+		if(d4 >= 5 || d4 <= 0) {
+			System.out.println("B");
+		}
+		int d5 = 11;
+		if(d5 > 8) {
+			System.out.println("A");
+		}else if(d5 < 10) {
+			System.out.println("B");
+		}
+		int d6 = 3;
+		if(d6 > 5) {
+			System.out.println("A");
+		}else {
+			System.out.println("Z");
+		}
+		int d7 = 20;
+		if(d7 < 8) {
+			System.out.println("A");
+		}else if(a == 8) {
+			System.out.println("B");
+		}else {
+			System.out.println("C");
+		}
+		int age = 35;
+		if(age < 20) {
+			System.out.println("未成年");
+		}else if(age >= 20 && age <= 80) {
+			System.out.println("成人");
+		}else {
+			System.out.println("高齢者");
+		}
+		int d8 = 5;
+		if(d8 % 2 == 0) {
+			System.out.println("偶数");
+		}else {
+			System.out.println("奇数");
+		}
+		
+		int e1 = 20;
+		if(e1 >10) {
+			if(e1 < 30) {
+				System.out.println("A");
+			}
+		}
+		int e2 = 7;
+		if(e2 > 3) {
+			if(e2 < 10) {
+				System.out.println("A");
+			}
+		}
+		int e3 = 10;
+		if(e3 > 0) {
+			if(e3 == 2) {
+				System.out.println("A");
+			}else if(e3 == 3) {
+				System.out.println("B");
+			}else {
+				System.out.println("C");
+			}
+		}
+		int number = 88;
+		if(number <= 100) {
+			if(number < 20) {
+				System.out.println("未成年");
+			}else if(number == 77) {
+				System.out.println("喜寿");
+			}else if(number == 88) {
+				System.out.println("米寿");
+			}else {
+				System.out.println("成人");
+			}
+		}
+		int number1 = 110;
+		if(number1 <= 100) {
+			if(number1 < 20) {
+				System.out.println("未成年");
+			}else if(number1 == 77) {
+				System.out.println("喜寿");
+			}else if(number1 == 88) {
+				System.out.println("米寿");
+			}else {
+				System.out.println("成人");
+			}
+		}
+		
+		
 
 	}
 
